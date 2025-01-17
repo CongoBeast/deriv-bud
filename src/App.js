21821c127d1c1d6075aa89c52063ee88e6a30766
Methembe Tshuma <53082789+CongoBeast@users.noreply.github.com>
2025-01-17 07:21:22 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import logo from './logo.svg';
+import {BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
 import './App.css';
+import HomePage from "./pages/HomePage";
+import TopNavBar from "./components/TopNavBar";
+import Sidebar from "./components/sidebar";
+import TradesTable from './pages/TradesTable';
+import Dashboard from './pages/Dashboard';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <Router>
+
+    <div className="d-flex">
+      <Sidebar />
+
+      <div className="container-fluid">
+      <TopNavBar />
+
+      <Routes>
+         <Route path="/" element={<HomePage />} />
+         <Route path="/mytrades" element={<TradesTable />} />
+         <Route path="/analysis" element={<Dashboard />} />
+
+      </Routes>
+
+      </div>
+
     </div>
+
+      
+    </Router>
   );
 }
 
