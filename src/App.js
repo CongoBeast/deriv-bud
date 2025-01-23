d6dd19b5e76e1f4f9f5a92cb685291e01366fdb1
Methembe Tshuma <53082789+CongoBeast@users.noreply.github.com>
2025-01-23 15:49:37 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -6,8 +6,11 @@ import TopNavBar from "./components/TopNavBar";
 import Sidebar from "./components/sidebar";
 import TradesTable from './pages/TradesTable';
 import Dashboard from './pages/Dashboard';
+import AuthPage from './pages/AuthPage';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import SignalsPage from './pages/SignalsPage';
+import AddSignalPage from './pages/AddSignalsPage';
 
 
 function App() {
@@ -25,6 +28,10 @@ function App() {
          <Route path="/" element={<HomePage />} />
          <Route path="/mytrades" element={<TradesTable />} />
          <Route path="/analysis" element={<Dashboard />} />
+         <Route path="/auth" element={<AuthPage />} />
+         <Route path="/signals" element={<SignalsPage />} />
+         <Route path="/addsignals" element={<AddSignalPage />} />
+
 
       </Routes>
 
